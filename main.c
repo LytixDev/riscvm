525ec0b36d2fcc57dca67061373f2c6197da9e2c
Nicolai Brand <nicolaibrand2002@gmail.com>
2025-05-26 16:24:40 +0200

    skeleton

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
+/*
+ *  Copyright (C) 2025 Nicolai Brand (lytix.dev)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include "types.h"
-
-
-// Types
-// R-type: Register–Register (e.g., add, sub)
-// I-type: Immediate (e.g., addi, lw)
-// S-type: Store instructions (e.g., sw)
-// B-type: Conditional branches (e.g., beq, bne)
-// U-type: Upper immediate (e.g., lui)
-// J-type: Unconditional jumps (e.g., jal)
 
 typedef enum {
     REG_ZERO = 0, // x0
@@ -62,9 +69,8 @@ typedef enum {
     //OPCODE_SYSTEM    = 0b1110011  // I-type: CSR access, ecall, ebreak
 } Opcode;
 
-
 typedef enum {
-    // For OPCODE_BRANCH (0b1100011)
+    /* OPCODE_BRANCH */
     FUNCT3_BEQ  = 0b000,
     FUNCT3_BNE  = 0b001,
     FUNCT3_BLT  = 0b100,
@@ -72,7 +78,7 @@ typedef enum {
     FUNCT3_BLTU = 0b110,
     FUNCT3_BGEU = 0b111,
 
-    // For OPCODE_LOAD / OPCODE_STORE
+    /* OPCODE_LOAD and OPCODE_STORE */
     FUNCT3_LB   = 0b000,
     FUNCT3_LH   = 0b001,
     FUNCT3_LW   = 0b010,
@@ -86,7 +92,7 @@ typedef enum {
     FUNCT3_SW   = 0b010,
     FUNCT3_SD   = 0b011,
 
-    // For OPCODE_OP and OPCODE_OP_IMM
+    /* OPCODE_OP and OPCODE_OP_IMM */
     FUNCT3_ADD_SUB  = 0b000,
     FUNCT3_SLL      = 0b001,
     FUNCT3_SLT      = 0b010,
@@ -96,10 +102,10 @@ typedef enum {
     FUNCT3_OR       = 0b110,
     FUNCT3_AND      = 0b111,
 
-    // For JALR
+    /* JALR */
     FUNCT3_JALR     = 0b000,
 
-    // // For SYSTEM
+    // /* SYSTEM */
     // FUNCT3_PRIV     = 0b000,
     // FUNCT3_CSRRW    = 0b001,
     // FUNCT3_CSRRS    = 0b010,
@@ -108,13 +114,13 @@ typedef enum {
     // FUNCT3_CSRRSI   = 0b110,
     // FUNCT3_CSRRCI   = 0b111,
 
-    // // For FENCE
+    // /* FENCE */
     // FUNCT3_FENCE    = 0b000,
     // FUNCT3_FENCE_I  = 0b001
 } Funct3;
 
 typedef enum {
-    // OPCODE_OP / OPCODE_OP_32
+    /* OPCODE_OP and OPCODE_OP_32 */
     FUNCT7_ADD      = 0b0000000,
     FUNCT7_SUB      = 0b0100000,
     FUNCT7_SLL      = 0b0000000,
@@ -126,49 +132,155 @@ typedef enum {
     FUNCT7_OR       = 0b0000000,
     FUNCT7_AND      = 0b0000000,
 
-    // RV64-only instructions (OP_32)
+    // TODO: What is a word in riscv64 ??
     FUNCT7_ADDW     = 0b0000000,
     FUNCT7_SUBW     = 0b0100000,
     FUNCT7_SLLW     = 0b0000000,
     FUNCT7_SRLW     = 0b0000000,
     FUNCT7_SRAW     = 0b0100000,
 
-    // SYSTEM instructions (ecall, ebreak, mret, etc.)
+    /* SYSTEM instructions (ecall, ebreak, mret, etc.) */
     //FUNCT7_SYSTEM   = 0b0000000
 } Funct7;
 
 typedef struct {
     s64 regs[32];
     u64 pc;
-    u64 cycles;
+    u64 steps;
 } RiscVM;
 
+
+static u32 encode_rtype(u8 rd, u8 rs1, u8 rs2, u8 opcode, u8 funct7, u8 funct3) {
+    return ((funct7 & 0x7F) << 25) |
+           ((rs2 & 0x1F) << 20)    |
+           ((rs1 & 0x1F) << 15)    |
+           ((funct3 & 0x07) << 12) |
+           ((rd & 0x1F) << 7)      |
+           (opcode & 0x7F);
+}
 
 
 void execute_instruction(RiscVM *vm, u32 inst)
 {
+    vm->steps += 1;
     u8 opcode = inst & 0x7F;
-    u8 rd = (inst >> 7) & 0x1F;
-    u8 funct3 = (inst >> 12) & 0x7;
-    u8 rs1 = (inst >> 15) & 0x1F;
-    u8 rs2 = (inst >> 20) & 0x1F;
-    u8 funct7 = (inst >> 25) & 0x7F;
 
-    if (opcode == OPCODE_OP && funct3 == FUNCT3_ADD_SUB && funct7 == FUNCT7_ADD) {
-        vm->regs[rd] = vm->regs[rs1] + vm->regs[rs2];
+    switch (opcode) {
+    default:
+        printf("Unknown opcode: 0x%02X\n", opcode);
+        break;
+    /* R-type (register-register) */
+    case OPCODE_OP: {
+        vm->pc += 4;
+        u8 rd = (inst >> 7)  & 0x1F;
+        u8 funct3 = (inst >> 12) & 0x07;
+        u8 rs1 = (inst >> 15) & 0x1F;
+        u8 rs2 = (inst >> 20) & 0x1F;
+        u8 funct7 = (inst >> 25) & 0x7F;
+
+        switch (funct3) {
+        case FUNCT3_ADD_SUB:
+            if (funct7 == FUNCT7_ADD) {
+                vm->regs[rd] = vm->regs[rs1] + vm->regs[rs2];
+            } else if (funct7 == FUNCT7_SUB) {
+                vm->regs[rd] = vm->regs[rs1] - vm->regs[rs2];
+            }
+            break;
+        case FUNCT3_AND:
+            vm->regs[rd] = vm->regs[rs1] & vm->regs[rs2];
+            break;
+        // TODO: other R-type ops
+        }
+        break;
+    }
+    /* I-type (immediate) */
+    case OPCODE_OP_IMM: {
+        vm->pc += 4;
+        u8 rd = (inst >> 7)  & 0x1F;
+        u8 funct3 = (inst >> 12) & 0x07;
+        u8 rs1 = (inst >> 15) & 0x1F;
+        s32 imm  = (s32)inst >> 20; // sign-extended
+
+        switch (funct3) {
+        case FUNCT3_ADD_SUB:
+            vm->regs[rd] = vm->regs[rs1] + imm;
+            break;
+        // TODO: slli, srli, etc.
+        }
+        break;
+    }
+    /* I-type load */
+    case OPCODE_LOAD: {
+        u8 rd = (inst >> 7)  & 0x1F;
+        u8 funct3 = (inst >> 12) & 0x07;
+        u8 rs1 = (inst >> 15) & 0x1F;
+        s32 imm = (s32)inst >> 20;
+
+        // TODO read from memory
+        break;
+    }
+    /* S-type (store instructions) */
+    case OPCODE_STORE: {
+        vm->pc += 4;
+        u8 imm4_0 = (inst >> 7)  & 0x1F;
+        u8 funct3 = (inst >> 12) & 0x07;
+        u8 rs1 = (inst >> 15) & 0x1F;
+        u8 rs2 = (inst >> 20) & 0x1F;
+        u8 imm11_5 = (inst >> 25) & 0x7F;
+        s32 imm = (imm11_5 << 5) | imm4_0;
+        // sign-extend
+        if (imm & (1 << 11)) {
+            imm |= ~((1 << 12) - 1);
+        }
+
+        // TODO: write to memory
+        break;
+    }
+    /* B-type (conditional branch instructions) */
+    case OPCODE_BRANCH: {
+        vm->pc += 4;
+        u8 funct3 = (inst >> 12) & 0x07;
+        u8 rs1    = (inst >> 15) & 0x1F;
+        u8 rs2    = (inst >> 20) & 0x1F;
+
+        // Immediate is weirdly encoded
+        s32 imm = ((inst >> 31) << 12) |
+                  (((inst >> 7) & 0x1) << 11) |
+                  (((inst >> 25) & 0x3F) << 5) |
+                  (((inst >> 8) & 0xF) << 1);
+        // sign-extend
+        if (imm & (1 << 12)) {
+            imm |= ~((1 << 13) - 1);
+        }
+
+        if (funct3 == FUNCT3_BEQ && vm->regs[rs1] == vm->regs[rs2]) {
+            vm->pc += imm;
+        } else {
+            vm->pc += 4;
+        }
+
+        break;
     }
 
-    // vm->pc += 4;
-    // vm->cycles += 1;
+    // TODO: U and J types: OPCODE_JAL, OPCODE_JALR, OPCODE_LUI, and more
+
+    }
 }
 
 int main(void)
 {
+    // TODO: Handling invalid instruction encoding
+    //       Memory
     RiscVM vm = {0};
-    // add x1, x2, x3
     vm.regs[2] = 10;
     vm.regs[3] = 20;
-    u32 inst = 0b00000000001100010000000010110011;
-    execute_instruction(&vm, inst);
+    u32 add_inst = encode_rtype(1, 2, 3, OPCODE_OP, FUNCT7_ADD, FUNCT3_ADD_SUB);
+    u32 sub_inst = encode_rtype(1, 1, 2, OPCODE_OP, FUNCT7_SUB, FUNCT3_ADD_SUB);
+    // 30 = 10 + 20
+    execute_instruction(&vm, add_inst);
+    // 20 = 30 - 10
+    execute_instruction(&vm, sub_inst);
+
     printf("%zu\n", vm.regs[1]);
 }
+
